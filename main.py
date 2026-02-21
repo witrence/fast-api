7625912d66d78739879bb7d0adc763abc012acae
Kapil <kb@cadana.dk>
2026-02-21 13:47:00 +0530

    save

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from fast-api!")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/", response_class=HTMLResponse)
+async def custom(request: Request):
+    # print(request)
+    return "<h2>Heyyyyyyyyyy</h2>"
+    return {
+        "status": "OK", 
+        "client": request.client.host,
+        "method": request.method,
+        "url": str(request.url),
+        "client_ip": request.client.host,
+        "headers": dict(request.headers),
+        "query": dict(request.query_params),
+        "path": request.path_params,
+        "cookies": request.cookies,
+        "body": await request.body(),
+        # "json": await request.json(),
+    }
